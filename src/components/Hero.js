2dbd34a8c55d2e48f43ba68520f3adb7e099f208
Amine ZEGMOU <amine95170@gmail.com>
2021-04-22 17:34:19 +0200

    some modif

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,16 +6,18 @@ const Hero = () => {
     <section className="hero">
       <div className="hero-wrap container">
         <div className="hero-title title-1">
-          <h1>DÉVELOPPEUR</h1>
+          <h1>AMINE</h1>
         </div>
         <div className="hero-title title-2">
-          <div className="hero-title-wrap">
-            <span>HETIC 2021</span>
-          </div>
-          <h1>FRONT-END</h1>
+          {/*<div className="hero-title-wrap">*/}
+          {/*  <span>HETIC 2021</span>*/}
+          {/*</div>*/}
+          <h1>
+            DÉVELOPPEUR <br /> FRONT-END
+          </h1>
         </div>
         <div className="hero-title title-3">
-          <h1>ET DESIGNER.</h1>
+          <h1>ZEGMOU</h1>
         </div>
       </div>
     </section>
